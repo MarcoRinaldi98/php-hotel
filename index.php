7547cada2045b14e922b03ec2cbabb3b467cca82
Marco Rinaldi <marcorinaldi98@libero.it>
2023-04-18 14:04:25 +0200

    consegna base completata

--- index.php
+++ index.php
@@ -10,48 +10,81 @@
     <title>Hotel</title>
 </head>
 <body>
-    <?php
-        $hotels = [
+    <table class="table table-dark container-lg">
+        <thead>
+            <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Parking</th>
+            <th scope="col">Vote</th>
+            <th scope="col">Distance</th>
+            </tr>
+        </thead>
+        <tbody>
 
-            [
-                'name' => 'Hotel Belvedere',
-                'description' => 'Hotel Belvedere Descrizione',
-                'parking' => true,
-                'vote' => 4,
-                'distance_to_center' => 10.4
-            ],
-            [
-                'name' => 'Hotel Futuro',
-                'description' => 'Hotel Futuro Descrizione',
-                'parking' => true,
-                'vote' => 2,
-                'distance_to_center' => 2
-            ],
-            [
-                'name' => 'Hotel Rivamare',
-                'description' => 'Hotel Rivamare Descrizione',
-                'parking' => false,
-                'vote' => 1,
-                'distance_to_center' => 1
-            ],
-            [
-                'name' => 'Hotel Bellavista',
-                'description' => 'Hotel Bellavista Descrizione',
-                'parking' => false,
-                'vote' => 5,
-                'distance_to_center' => 5.5
-            ],
-            [
-                'name' => 'Hotel Milano',
-                'description' => 'Hotel Milano Descrizione',
-                'parking' => true,
-                'vote' => 2,
-                'distance_to_center' => 50
-            ],
+            <?php
+                $hotels = [
+                    [
+                        'name' => 'Hotel Belvedere',
+                        'description' => 'Hotel Belvedere Descrizione',
+                        'parking' => true,
+                        'vote' => 4,
+                        'distance_to_center' => 10.4
+                    ],
+                    [
+                        'name' => 'Hotel Futuro',
+                        'description' => 'Hotel Futuro Descrizione',
+                        'parking' => true,
+                        'vote' => 2,
+                        'distance_to_center' => 2
+                    ],
+                    [
+                        'name' => 'Hotel Rivamare',
+                        'description' => 'Hotel Rivamare Descrizione',
+                        'parking' => false,
+                        'vote' => 1,
+                        'distance_to_center' => 1
+                    ],
+                    [
+                        'name' => 'Hotel Bellavista',
+                        'description' => 'Hotel Bellavista Descrizione',
+                        'parking' => false,
+                        'vote' => 5,
+                        'distance_to_center' => 5.5
+                    ],
+                    [
+                        'name' => 'Hotel Milano',
+                        'description' => 'Hotel Milano Descrizione',
+                        'parking' => true,
+                        'vote' => 2,
+                        'distance_to_center' => 50
+                    ]
+                ];
 
-        ];
-    ?>
-    
+                foreach($hotels as $element) {
+                    if ($element['parking']) {
+                        echo "<tr>
+                        <th scope='row'>" . $element['name'] . "</th>
+                        <td>" . $element['description'] . "</td>
+                        <td> Si </td>
+                        <td>" . $element['vote'] . "</td>
+                        <td>" . $element['distance_to_center'] . "</td>
+                        </tr>";
+                    } else {
+                        echo "<tr>
+                        <th scope='row'>" . $element['name'] . "</th>
+                        <td>" . $element['description'] . "</td>
+                        <td> No </td>
+                        <td>" . $element['vote'] . "</td>
+                        <td>" . $element['distance_to_center'] . "</td>
+                        </tr>";
+                    }
+                }
+            ?>
+
+        </tbody>
+    </table>
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
